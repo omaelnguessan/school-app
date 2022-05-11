6ef41a75306589350b46a1dc585a7922f7d36927
N'GUESSAN Ahouatho Omaël <omaelberiz@outlook.fr>
2022-05-11 14:40:17 +0000

    ADD : Lesson module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), LessonModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost:27017/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+    StudentModule,
+  ],
 })
 export class AppModule {}
